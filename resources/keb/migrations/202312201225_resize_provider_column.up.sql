9f1b4cad9759bc9e3d676336da79e1bb5964dab3
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2024-03-04 10:35:43 +0100

    Cleanup gh-pages branch

--- resources/keb/migrations/202312201225_resize_provider_column.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE instances
-    ALTER COLUMN provider TYPE varchar(255);
--- resources/keb/migrations/202313201225_resize_provider_column.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE instances
-    ALTER COLUMN provider TYPE varchar(16) USING substring(provider, 1, 16);
