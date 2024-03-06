2219348df17fce7ac6ad88454589bf814818dd92
ralikio <74771103+ralikio@users.noreply.github.com>
2024-03-06 13:29:34 +0100

    Profiler Removal

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -221,35 +221,13 @@ func periodicProfile(logger lager.Logger, profiler ProfilerConfig) {
 }
 
 func main() {
-	f, err := os.Create("profile.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	if err := pprof.StartCPUProfile(f); err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-
-	traceFile, err := os.Create("trace.out")
-	if err != nil {
-		panic(err)
-	}
-	defer traceFile.Close()
-
-	if err := trace.Start(traceFile); err != nil {
-		panic(err)
-	}
-	defer trace.Stop()
-
 	apiextensionsv1.AddToScheme(scheme.Scheme)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// create and fill config
 	var cfg Config
-	err = envconfig.InitWithPrefix(&cfg, "APP")
+	err := envconfig.InitWithPrefix(&cfg, "APP")
 	fatalOnError(err)
 
 	// check default Kyma versions
