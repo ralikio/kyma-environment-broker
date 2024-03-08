d0ac8ad870632d9886b863398e71ea3bce7b571a
ralikio <74771103+ralikio@users.noreply.github.com>
2024-03-08 15:14:35 +0100

    Disable Periodic Profiler

--- internal/profiler/profiler.go
+++ internal/profiler/profiler.go
@@ -45,28 +45,28 @@ type Profiler struct {
 }
 
 func (p *Profiler) PeriodicProfile() {
-	if p.config.Memory == false {
-		return
-	}
-	p.logger.Info(fmt.Sprintf("Starting periodic profiler %v", p.config))
-	if err := os.MkdirAll(p.config.Path, os.ModePerm); err != nil {
-		p.logger.Error(fmt.Sprintf("Failed to create dir %v for profile storage", p.config.Path), err)
-	}
-	for {
-		profName := fmt.Sprintf("%v/mem-%v.pprof", p.config.Path, time.Now().Unix())
-		p.logger.Info(fmt.Sprintf("Creating periodic memory profile %v", profName))
-		profFile, err := os.Create(profName)
-		if err != nil {
-			p.logger.Error(fmt.Sprintf("Creating periodic memory profile %v failed", profName), err)
-		}
-		err = pprof.Lookup("allocs").WriteTo(profFile, 0)
-		if err != nil {
-			p.logger.Error(fmt.Sprintf("Error while looking and writing allocs with profile %v", profName), err)
-		}
+	// if p.config.Memory == false {
+	// 	return
+	// }
+	// p.logger.Info(fmt.Sprintf("Starting periodic profiler %v", p.config))
+	// if err := os.MkdirAll(p.config.Path, os.ModePerm); err != nil {
+	// 	p.logger.Error(fmt.Sprintf("Failed to create dir %v for profile storage", p.config.Path), err)
+	// }
+	// for {
+	// 	profName := fmt.Sprintf("%v/mem-%v.pprof", p.config.Path, time.Now().Unix())
+	// 	p.logger.Info(fmt.Sprintf("Creating periodic memory profile %v", profName))
+	// 	profFile, err := os.Create(profName)
+	// 	if err != nil {
+	// 		p.logger.Error(fmt.Sprintf("Creating periodic memory profile %v failed", profName), err)
+	// 	}
+	// 	err = pprof.Lookup("allocs").WriteTo(profFile, 0)
+	// 	if err != nil {
+	// 		p.logger.Error(fmt.Sprintf("Error while looking and writing allocs with profile %v", profName), err)
+	// 	}
 
-		gruntime.GC()
-		time.Sleep(p.config.Sampling)
-	}
+	// 	gruntime.GC()
+	// 	time.Sleep(p.config.Sampling)
+	// }
 }
 
 func (p *Profiler) StartIfSwitched() {
