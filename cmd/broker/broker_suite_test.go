cb4e8d40fe217fa31ed3c9a0f942d1999cb82e15
ralikio <74771103+ralikio@users.noreply.github.com>
2024-05-31 16:32:05 +0200

    E2E Tests

--- cmd/broker/broker_suite_test.go
+++ cmd/broker/broker_suite_test.go
@@ -196,6 +196,7 @@ func NewBrokerSuiteTestWithConfig(t *testing.T, cfg *Config, version ...string) 
 			URL:                         "http://localhost",
 			DefaultGardenerShootPurpose: "testing",
 			DefaultTrialProvider:        internal.AWS,
+			EnableShootAndSeedRegionValidation: cfg.Provisioner.EnableShootAndSeedRegionValidation,
 		}, defaultKymaVer, map[string]string{"cf-eu10": "europe", "cf-us10": "us"}, cfg.FreemiumProviders, defaultOIDCValues())
 
 	storageCleanup, db, err := GetStorageForE2ETests()
