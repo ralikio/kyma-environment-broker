3a36800fd382228ed87b156c2f42039b3e915c40
ralikio <74771103+ralikio@users.noreply.github.com>
2024-06-10 14:14:46 +0200

    Linting

--- internal/utils/yaml_test.go
+++ internal/utils/yaml_test.go
@@ -10,34 +10,33 @@ import (
 func TestUnmarshalingFromYamlFile(t *testing.T) {
 
 	tests := []struct {
-		name string
+		name     string
 		filename string
 		expected interface{}
-		data interface{}
+		data     interface{}
 	}{
 		{
-			name: "correctly unmarshals yaml file with list of strings",
+			name:     "correctly unmarshals yaml file with list of strings",
 			filename: "testdata/list_of_string_in_property.yaml",
-			data: &map[string][]string{},
+			data:     &map[string][]string{},
 			expected: &map[string][]string{"prop": {"string-1", "string-2"}},
 		},
 		{
-			name: "correctly unmarshals yaml file with embedded object",
+			name:     "correctly unmarshals yaml file with embedded object",
 			filename: "testdata/embedded_object.yaml",
-			data: &map[string]interface{}{},
-			expected: &map[string]interface{}{"obj": map[interface{}]interface{}{"prop1": 
-				map[interface{}]interface{} {"another_obj": map[interface{}]interface{}{"prop2": "value2", "prop3":"value3"}}}},
+			data:     &map[string]interface{}{},
+			expected: &map[string]interface{}{"obj": map[interface{}]interface{}{"prop1": map[interface{}]interface{}{"another_obj": map[interface{}]interface{}{"prop2": "value2", "prop3": "value3"}}}},
 		},
 		{
-			name: "correctly unmarshals yaml file with mapping of strings to list of strings",
+			name:     "correctly unmarshals yaml file with mapping of strings to list of strings",
 			filename: "testdata/multiple_mappings.yaml",
-			data: &map[string][]string{},
+			data:     &map[string][]string{},
 			expected: &map[string][]string{"key1": {"value1", "value2"}, "key2": {"value3", "value4"}},
 		},
 		{
-			name: "overrides already defined keys wile unmarshalling yaml file with mapping of strings to list of strings",
+			name:     "overrides already defined keys wile unmarshalling yaml file with mapping of strings to list of strings",
 			filename: "testdata/multiple_mappings_override.yaml",
-			data: &map[string][]string{},
+			data:     &map[string][]string{},
 			expected: &map[string][]string{"key1": {"value5", "value6"}, "key2": {"value3", "value4"}},
 		},
 	}
@@ -45,11 +44,10 @@ func TestUnmarshalingFromYamlFile(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			// given/when
 			err := UnmarshalYamlFile(tt.filename, tt.data)
-	
+
 			// then
 			require.NoError(t, err)
 			assert.Equal(t, tt.expected, tt.data)
 		})
 	}
 }
-
