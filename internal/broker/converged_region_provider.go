fc0679143dfd635b6057ed8887ecff84e2d3cda6
ralikio <74771103+ralikio@users.noreply.github.com>
2024-06-12 12:46:40 +0200

    Initialization with Region Configuration

--- internal/broker/converged_region_provider.go
+++ internal/broker/converged_region_provider.go
@@ -1,11 +1,32 @@
 package broker
 
+import (
+	"fmt"
+)
+
+//go:generate mockery --name=RegionReader --output=automock --outpkg=automock --case=underscore
+type RegionReader interface {
+	Read(filename string) (map[string][]string, error)
+}
+
 type ConvergedCloudRegionProvider interface {
 	GetRegions() []string
 }
 
 type PathBasedConvergedCloudRegionsProvider struct {
 	// placeholder
+	regionConfiguration map[string][]string
+}
+
+func NewPathBasedConvergedCloudRegionsProvider(regionConfigurationPath string, reader RegionReader) (*PathBasedConvergedCloudRegionsProvider, error) {
+	regionConfiguration, err := reader.Read(regionConfigurationPath)
+	if err != nil {
+		return nil, fmt.Errorf("while unmarshalling a file with sap-converged-cloud region mappings: %w", err)
+	}
+
+	return &PathBasedConvergedCloudRegionsProvider{
+		regionConfiguration: regionConfiguration,
+	}, nil
 }
 
 func (c *PathBasedConvergedCloudRegionsProvider) GetRegions() []string {
--- internal/broker/converged_region_provider_test.go
+++ internal/broker/converged_region_provider_test.go
@@ -3,10 +3,11 @@ package broker
 import (
 	"testing"
 
+	"github.com/kyma-project/kyma-environment-broker/internal/broker/automock"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConvergedCloudRegions_GetDefaultRegions(t *testing.T) {
+func TestOneForAllConvergedCloudRegionsProvider_GetDefaultRegions(t *testing.T) {
 	// given
 	c := &OneForAllConvergedCloudRegionsProvider{}
 
@@ -15,4 +16,22 @@ func TestConvergedCloudRegions_GetDefaultRegions(t *testing.T) {
 
 	// then
 	assert.Equal(t, []string{"eu-de-1"}, result)
+}
+
+func TestPathBasedConvergedCloudRegionsProvider_FactoryMethod(t *testing.T) {
+	// given
+	configLocation := "path-to-config"
+	regions := map[string][]string{
+		"eu-de-1": {"eu-de-1"},
+	}
+
+	mockReader := automock.NewRegionReader(t)
+	mockReader.On("Read", configLocation).Return(regions, nil)
+	
+	// when
+	provider, err := NewPathBasedConvergedCloudRegionsProvider(configLocation, mockReader)
+
+	// then
+	assert.NoError(t, err)
+	assert.Equal(t, regions, provider.regionConfiguration)
 }
