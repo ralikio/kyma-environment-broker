0569b73f771cd9d6e04f4912103450210773dbd3
ralikio <74771103+ralikio@users.noreply.github.com>
2024-10-03 10:38:43 +0200

    Attributes Correction

--- resources/keb/migrations/202009171000_add_kyma_bindings.up.sql
+++ resources/keb/migrations/202009171000_add_kyma_bindings.up.sql
@@ -2,11 +2,13 @@ CREATE TABLE IF NOT EXISTS bindings (
     id VARCHAR(255) NOT NULL,
     runtime_id VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
-    expires_at TIMESTAMPTZ NOT NULL,
     -- represents algorithm used to generate a kubeconfig, initialy: adminkubeconfig or tokenrequest
     type TEXT, 
     -- content of the kubeconfig
-	config TEXT, 
+	kubeconfig TEXT, 
+    -- expiration seconds
+    expiration_seconds INTEGER,
     -- allow for the same binding id to be used for different runtimes
-    PRIMARY KEY(id, runtime_id) 
+    PRIMARY KEY(id, runtime_id), 
+    version integer NOT NULL
 );
